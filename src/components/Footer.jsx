c4061bd313a308c7b700f1dc28e98087995c308a
sammrguez <sammrguez@gmail.com>
2024-03-26 15:59:00 -0600

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import facebook from '../images/facebook.png';
+import instagram from '../images/instagram.png';
+import logoBlack from '../images/LOGO_IRNI_short.png';
 
 function Footer() {
   return (
     <section className='footer'>
-      <img className='footer__logo' />
+      <img src={logoBlack} className='footer__logo' />
       <ul className='footer__social-media'>
         <li className='footer__item'>
-          <Link>
-            <img src={facebook} />
-            <h4> I really Need It</h4>
+          <Link
+            className='footer__link'
+            to='https://www.facebook.com/IReallyNeedItMx?locale=eo_EO'
+          >
+            <img src={facebook} className='footer__media-icon' />
+            <h4 className='footer_media-text'> I really Need It</h4>
           </Link>
         </li>
         <li className='footer__item'>
-          <Link>
-            <img src={facebook} />
-            <h4> I_really_Need_It_mx</h4>
+          <Link
+            className='footer__link'
+            to='https://www.instagram.com/i_really_need_it_mx/'
+          >
+            <img src={instagram} className='footer__media-icon' />
+            <h4 className='footer_media-text'> I_really_Need_It_mx</h4>
           </Link>
         </li>
       </ul>
