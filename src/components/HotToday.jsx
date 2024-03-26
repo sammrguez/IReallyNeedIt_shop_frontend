6e54f933a9846be877b466bc41134685a58cd0ee
sammrguez <sammrguez@gmail.com>
2024-03-26 11:44:59 -0600

    start with hot today

--- src/components/HotToday.jsx
+++ src/components/HotToday.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import texture from '../images/textura_IRNI.jpg';
+import mug from '../images/overthinker.jpg';
 
 function HotToday() {
   return (
     <section className='hotToday'>
       <img className='hotToday__decoration-band' src={texture} />
-      <div className='hotToday__images'>
-        <div className='hotToday__image-frame'></div>
-        <div className='hotToday__image-container'>
-          <img className='hotToday__image' />
+      <div className='HotToday__content'>
+        <div className='hotToday__images'>
+          <div className='hotToday__image-frame'>
+            <div className='hotToday__image-container'>
+              <img className='hotToday__image' src={mug} />
+            </div>
+          </div>
+          <button className='button button_shop'>
+            <h3 className='button__text'></h3>
+          </button>
+        </div>
+        <div className='hotToday__data'>
+          <h2 className='hotToday__header'>Presentando el Producto del Mes</h2>
+          <h3 className='hotToday__text'>
+            ¡Los imprescindibles de la temporada están aquí! Explora lo más
+            destacado de hoy en '
+            <span className='hotToday__highlight'>I Really Need It</span>
+          </h3>
         </div>
       </div>
-      <div className='hotToday__data'></div>
     </section>
   );
 }
