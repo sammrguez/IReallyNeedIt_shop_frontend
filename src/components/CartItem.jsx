3e0910df41c743b787a36f3d8970313623eeb771
sammrguez <sammrguez@gmail.com>
2024-03-27 12:44:21 -0600

    cart Item css files

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import item from '../images/snoopy-airpods.png';
 
 function CartItem() {
   return (
     <li className='cartItem'>
-      <div className='cartItem__container'>
-        <img className='cartItem__image' />
+      <div className='cart__image-container'>
+        <img className='cartItem__image' src={item} />
+      </div>
+      <div className='cartItem__data'>
+        <h3 className='cartItem__header'>Case transparente para iphone</h3>
+        <p className='cartItem__price'>$299</p>
+        <div className='cartItem__quantityBox'></div>
       </div>
     </li>
   );
