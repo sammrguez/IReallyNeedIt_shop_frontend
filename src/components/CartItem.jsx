341a9270b8895822cc13679a1b63139992c4facb
sammrguez <sammrguez@gmail.com>
2024-04-12 09:57:47 -0600

    cart working

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -27,7 +27,10 @@ function CartItem({
       </div>
       <div className='cartItem__data'>
         <h3 className='cartItem__header'>{item.name}</h3>
-        <h3 className='cartItem__header'> {`modelo: ${selectedModel}`} </h3>
+        {selectedModel && (
+          <h3 className='cartItem__header'> {`modelo: ${selectedModel}`} </h3>
+        )}
+
         <p className='cartItem__price'>{`$ ${item.price}`}</p>
         <div className='cartItem__quantityBox-container'>
           <div className='cartItem__quantityBox'>
