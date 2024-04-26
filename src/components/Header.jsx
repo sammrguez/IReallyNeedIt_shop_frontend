571c1b38bc2fb5c9c413496b701436b606ce8b30
sammrguez <sammrguez@gmail.com>
2024-04-26 15:56:53 -0600

    working on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,20 +5,31 @@ import thermo from '../images/thermo-pretty.jpeg';
 function Header() {
   return (
     <section className='header'>
-      <div className='header__data'>
-        <p className='header__info'>
+      <div className='header__container'>
+        <h3 className='header__info'>
           ¿Alguna vez has sentido ese impulso irresistible de añadir un toque
           especial a tu vida cotidiana?
-        </p>
+        </h3>
+        <img className='header__image-png' src={snoopy} alt='Snoopy' />
+
         <h2 className='header__phrase'>
           ¡Entonces has llegado al lugar correcto!
         </h2>
-      </div>
-      <div className='header__images'>
-        <div className='header__hexagon'>
-          <img className='header__image-hexagon' src={thermo} alt='Snoopy' />
-        </div>
-        <img className='header__image-png' src={snoopy} alt='Snoopy' />
+        {/* <div className='header__data'>
+          <div className='header__info'>
+            ¿Alguna vez has sentido ese impulso irresistible de añadir un toque
+            especial a tu vida cotidiana?
+          </div>
+          <h2 className='header__phrase'>
+            ¡Entonces has llegado al lugar correcto!
+          </h2>
+        </div> */}
+        {/* <div className='header__images'>
+           <div className='header__hexagon'>
+            <img className='header__image-hexagon' src={thermo} alt='Snoopy' />
+          </div> 
+          <img className='header__image-png' src={snoopy} alt='Snoopy' />
+        </div> */}
       </div>
     </section>
   );
