7a0e9517ed65d88a31ecbc84934f878a6dfb094e
sammrguez <sammrguez@gmail.com>
2024-04-26 10:55:40 -0600

    fixing scrolling

--- src/components/CardPopup.jsx
+++ src/components/CardPopup.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { useState, useEffect, useContext } from 'react';
 
-function CardPopup({ selectedCard }) {
+function CardPopup({ selectedCard, onAddProductClick, onClose }) {
   const [selectedModel, setSelectedModel] = useState(null);
+
+  const handleModelSelection = (model) => {
+    setSelectedModel(model);
+  };
 
   const handleAddToCart = (event) => {
     event.preventDefault();
@@ -16,40 +20,45 @@ function CardPopup({ selectedCard }) {
   };
 
   return (
-    <div className='cardPopup'>
-      <div className='popup__image-container'>
-        <img className='popup__image' src={selectedCard['photo-link']} />
-      </div>
-      <div className='popup__data'>
-        <h2 className='popup__header'>{selectedCard.name}</h2>
-        <p className='popup__paragraph'>{selectedCard.description}</p>
+    <>
+      <div className='cardPopup'>
+        <div className='popup__image-container'>
+          <img className='popup__image' src={selectedCard['photo-link']} />
+        </div>
+        <div className='popup__data'>
+          <h2 className='popup__header'>{selectedCard.name}</h2>
+          <p className='popup__paragraph'>{selectedCard.description}</p>
 
-        {selectedCard.models.length > 0 && (
-          <>
-            <h3 className='popup__sub-header'>Modelo:</h3>
-            <ul className='popup__sizes'>
-              {selectedCard.models.map((model, index) => {
-                return (
-                  <li
-                    className={`popup__size ${
-                      selectedModel === model ? 'size-selected' : ''
-                    }`}
-                    key={index}
-                    onClick={() => handleModelSelection(model)}
-                  >
-                    {model}
-                  </li>
-                );
-              })}
-            </ul>
-          </>
-        )}
-        <h4 className='popup__price'>{`$ ${selectedCard.price}`}</h4>
-        <p className='popup__paragraph-disclaimer'>
-          Los gastos de envío se calculan en la pantalla de pago
-        </p>
+          {selectedCard.models.length > 0 && (
+            <>
+              <h3 className='popup__sub-header'>Modelo:</h3>
+              <ul className='popup__sizes'>
+                {selectedCard.models.map((model, index) => {
+                  return (
+                    <li
+                      className={`popup__size ${
+                        selectedModel === model ? 'size-selected' : ''
+                      }`}
+                      key={index}
+                      onClick={() => handleModelSelection(model)}
+                    >
+                      {model}
+                    </li>
+                  );
+                })}
+              </ul>
+            </>
+          )}
+          <h4 className='popup__price'>{`$ ${selectedCard.price}`}</h4>
+          <p className='popup__paragraph-disclaimer'>
+            Los gastos de envío se calculan en la pantalla de pago
+          </p>
+        </div>
       </div>
-    </div>
+      <button className='button button_type_shop' onClick={handleAddToCart}>
+        <h3 className='button__text'> agregar a carrito </h3>
+      </button>
+    </>
   );
 }
 
--- src/components/PopupProduct.jsx
+++ src/components/PopupProduct.jsx
@@ -8,21 +8,6 @@ function PopupProduct({ selectedCard, name, onClose, onAddProductClick }) {
   const cart = useContext(CartContext);
   const [selectedModel, setSelectedModel] = useState(null);
 
-  const handleAddToCart = (event) => {
-    event.preventDefault();
-    if (selectedCard.models.length < 1 || selectedModel) {
-      const productToAdd = { ...selectedCard, selectedModel: selectedModel };
-      onAddProductClick(productToAdd);
-      onClose();
-    } else {
-      console.log('no has seleccionado ningun modelo');
-    }
-  };
-
-  const handleModelSelection = (model) => {
-    setSelectedModel(model);
-  };
-
   return (
     <div
       className={`popup popup_type_${name} ${
@@ -31,10 +16,13 @@ function PopupProduct({ selectedCard, name, onClose, onAddProductClick }) {
     >
       <div className='overlay' onClick={onClose}></div>
       <div className='popup__container popup__container_type_product'>
-        {selectedCard && <CardPopup selectedCard={selectedCard} />}
-        <button className='button button_type_shop' onClick={handleAddToCart}>
-          <h3 className='button__text'> agregar a carrito </h3>
-        </button>
+        {selectedCard && (
+          <CardPopup
+            selectedCard={selectedCard}
+            onAddProductClick={onAddProductClick}
+            onClose={onClose}
+          />
+        )}
       </div>
     </div>
   );
