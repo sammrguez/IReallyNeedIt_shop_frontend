d2b5a04901328dbe9d1cbd998f24a458365fa50d
sammrguez <sammrguez@gmail.com>
2024-04-28 07:51:59 -0600

    header img

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import snoopy from '../images/snoopy-airpods.png';
 import thermo from '../images/thermo-pretty.jpeg';
+import back from '../images/Happy Days Clear Sticker.jpeg';
 
 function Header() {
   return (
     <section className='header'>
       <div className='header__container'>
-        <h3 className='header__info'>
-          ¿Alguna vez has sentido ese impulso irresistible de añadir un toque
-          especial a tu vida cotidiana?
-        </h3>
-        <img className='header__image-png' src={snoopy} alt='Snoopy' />
+        <img src={back} className='header__image-background'></img>
 
-        <h2 className='header__phrase'>
-          ¡Entonces has llegado al lugar correcto!
-        </h2>
         {/* <div className='header__data'>
           <div className='header__info'>
             ¿Alguna vez has sentido ese impulso irresistible de añadir un toque
