317ef03b9c197e72c676925bdc3477d79351f75d
sammrguez <sammrguez@gmail.com>
2024-05-07 13:49:38 -0600

    test

--- src/index.jsx
+++ src/index.jsx
@@ -6,9 +6,7 @@ import App from './components/App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
