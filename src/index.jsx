84aa63f7fc131151b5dd42dae1d87159dfe4e141
sammrguez <sammrguez@gmail.com>
2024-05-07 16:56:33 -0600

    browser working

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-
-import App from './components/App.jsx';
+//import { render } from 'react-dom'; // <- This is the correct import // statement for React version 17
 import './index.css';
+import App from './components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
